40bac68e54797bce0ddb5723921695e3391d5af4
EliseyBelka <46591130+EliseyBelka@users.noreply.github.com>
2019-02-07 02:56:55 +0700

    Игра про астероиды.
    На 5* времени не хватило.
    Вопрос остался по моменту - столкновение астероида и снаряда. Если астероидов более 1 как определить номер астероида с которым произошло столкновения, требуется для его перерисовки. В решение столкновение обработано  для одного астероида.
    В приложении 1 -архив zip со всеми файлами 2 все фалы проекта с кодом

---
+++ Asteroid.cs
@@ -0,0 +1,27 @@
+﻿using MyGame;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Level2Les1
+{
+    class Asteroid : BaseObject, ICollision
+    {
+        public int Power { get; set; }
+        public Asteroid(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            Power = 1;
+        }
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.FillEllipse(Brushes.Blue, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+        public override void Update()
+        {
+            Pos.X = Pos.X + 15;
+            if (Pos.X > Game.Width) Pos.X = 0;
+        }
+        public bool Collision(ICollision o) => o.Rect.IntersectsWith(this.Rect);
+        public Rectangle Rect => new Rectangle(Pos, Size);
+    }
+}
---
+++ Bullet.cs
@@ -0,0 +1,26 @@
+﻿using MyGame;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Level2Les1
+{
+    class Bullet : BaseObject, ICollision
+    {
+        public Bullet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.FillRectangle(Brushes.LawnGreen, Pos.X+ Size.Width/2, Pos.Y, Size.Width, Size.Height);
+        }
+        public override void Update()
+        {
+            Pos.X = Pos.X + 30;
+            if (Pos.X > Game.Width) Pos.X = 0;
+        }
+        public bool Collision(ICollision o) => o.Rect.IntersectsWith(this.Rect);
+        public Rectangle Rect => new Rectangle(Pos, Size);
+    }
+}
+
