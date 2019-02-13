e6e3cf227166e61d312357ab90874f0c1598a9f6
EliseyBelka <46591130+EliseyBelka@users.noreply.github.com>
2019-02-13 10:02:52 +0700

    Add files via upload
    Задания 1 и 2а выполнил самостоятельно. 2-с разобрался по готовому. Задание 3 пока не смог осмыслить. Буду разбираться. Не хватает теории и времени чтобы её почитать.

---
+++ Task2-c.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson4
+{
+    class Task2_c
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 20; i++) numbers.Add(rnd.Next(1, 7));
+            foreach (int i in numbers) Console.Write(" " + i);
+            Console.WriteLine();
+            foreach (int val in numbers.Distinct())
+            {
+              Console.WriteLine(val + " - " + numbers.Where(x => x == val).Count() + " раз");
+            }
+            Console.ReadKey();
+        }
+       
+    }
+}
